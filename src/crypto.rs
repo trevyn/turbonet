e7887d12c1f24a7920c6fe335cb5b2f8278eb8f7
trevyn <trevyn-git@protonmail.com>
2022-01-16 16:37:16 +0300

    refine peer heartbeat

--- src/crypto.rs
+++ src/crypto.rs
@@ -4,9 +4,9 @@ use rand_chacha::ChaCha20Rng;
 
 #[allow(dead_code)]
 pub struct KeyMaterial {
- secret_key: [u8; 32],
- public_key: [u8; 96],
- proof_of_possession: [u8; 48],
+ pub secret_key: [u8; 32],
+ pub public_key: [u8; 96],
+ pub proof_of_possession: [u8; 48],
 }
 
 impl KeyMaterial {
@@ -21,16 +21,16 @@ impl KeyMaterial {
   let sk = SecretKey::key_gen(&ikm, &[]).unwrap();
   let pk = sk.sk_to_pk();
 
-  dbg!(hex::encode(sk.to_bytes()));
-  dbg!(hex::encode(pk.compress()));
+  // dbg!(hex::encode(sk.to_bytes()));
+  // dbg!(hex::encode(pk.compress()));
 
   let dst = b"BLS_SIG_BLS12381G1_XMD:SHA-256_SSWU_RO_POP_";
   let msg = b"blst is such a blast";
   let sig = sk.sign(msg, dst, &[]);
   let pop = sk.sign(&pk.compress(), b"BLS_POP_BLS12381G1_XMD:SHA-256_SSWU_RO_POP_", &[]);
 
-  dbg!(hex::encode(pop.compress()));
-  dbg!(hex::encode(sig.compress()));
+  // dbg!(hex::encode(pop.compress()));
+  // dbg!(hex::encode(sig.compress()));
 
   let err = sig.verify(true, msg, dst, &[], &pk, true);
   assert_eq!(err, blst::BLST_ERROR::BLST_SUCCESS);
--- src/lib.rs
+++ src/lib.rs
@@ -4,43 +4,32 @@
 
 mod crypto;
 
-pub use crypto::KeyMaterial;
+use serde_with::serde_as;
 use turbocharger::backend;
+use turbosql::{select, Turbosql};
 
 gflags::define!(--turbonet_bootstrap_ip: &str);
 gflags::define!(--turbonet_bootstrap_port: u16 = 34254);
 gflags::define!(--turbonet_listen_port: u16 = 34254);
 gflags::define!(--turbonet_heartbeat_interval_seconds: u16 = 2);
 
-use turbosql::{select, Turbosql};
+fn now_millis() -> i64 {
+ std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_millis() as i64
+}
 
-#[derive(Turbosql, Default)]
-struct _Turbonet_Peers {
+// SELECT rowid,ip,last_request_ms,last_response_ms,build_id,substr(hex(crypto_box_public_key), 1, 8) AS cb_public FROM _turbonet_peer;
+#[derive(Turbosql, Default, Debug)]
+struct _Turbonet_Peer {
  rowid: Option<i64>,
  ip: Option<u32>,
  port: Option<u16>,
- last_seen_ms: Option<i64>,
+ last_request_ms: Option<i64>,
+ last_response_ms: Option<i64>,
  crypto_box_public_key: Option<[u8; 32]>,
  bls_public_key: Option<[u8; 96]>,
  bls_proof_of_possession: Option<[u8; 48]>,
  base_url: Option<String>,
  build_id: Option<String>,
-}
-
-impl From<SelfResult> for _Turbonet_Peers {
- fn from(item: SelfResult) -> Self {
-  _Turbonet_Peers {
-   ip: Some(item.ip),
-   // port: item.port,
-   // last_seen_ms: item.last_seen_ms,
-   crypto_box_public_key: Some(item.crypto_box_public_key),
-   // bls_public_key: item.bls_public_key,
-   // bls_proof_of_possession: item.bls_proof_of_possession,
-   base_url: item.base_url,
-   build_id: Some(item.build_id),
-   ..Default::default()
-  }
- }
 }
 
 #[derive(Turbosql, Default, Debug)]
@@ -58,53 +47,89 @@ struct _Turbonet_Self {
 }
 
 impl _Turbonet_Self {
- fn generate() -> Self {
+ fn generate_keys() -> Self {
   let mut rng = crypto_box::rand_core::OsRng;
   let crypto_box_secret_key = crypto_box::SecretKey::generate(&mut rng);
+
+  let bls = crypto::KeyMaterial::generate_new();
 
   _Turbonet_Self {
    crypto_box_secret_key: Some(crypto_box_secret_key.as_bytes().to_owned()),
    crypto_box_public_key: Some(crypto_box_secret_key.public_key().as_bytes().to_owned()),
+   bls_secret_key: Some(bls.secret_key),
+   bls_public_key: Some(bls.public_key),
+   bls_proof_of_possession: Some(bls.proof_of_possession),
    ..Default::default()
   }
  }
 }
 
-#[backend]
-#[derive(PartialEq)]
-struct SelfResult {
- ip: u32,
- // port: u16,
- crypto_box_public_key: [u8; 32],
- // bls_public_key: [u8; 96],
- // bls_proof_of_possession: [u8; 48],
- base_url: Option<String>,
- build_id: String,
-}
-
-impl From<_Turbonet_Self> for SelfResult {
- fn from(item: _Turbonet_Self) -> Self {
-  SelfResult {
-   ip: 2130706433,
-   // port: item.port.unwrap(),
+impl From<_Turbonet_Self> for SelfResponse {
+ fn from(item: _Turbonet_Self) -> SelfResponse {
+  SelfResponse {
    crypto_box_public_key: item.crypto_box_public_key.unwrap(),
-   // bls_public_key: self.bls_public_key,
-   // bls_proof_of_possession: self.bls_proof_of_possession,
+   bls_public_key: item.bls_public_key.unwrap(),
+   bls_proof_of_possession: item.bls_proof_of_possession.unwrap(),
    base_url: item.base_url,
    build_id: item.build_id.unwrap(),
   }
  }
 }
 
+#[serde_as]
 #[backend]
-pub async fn turbonet_self() -> SelfResult {
+#[derive(PartialEq, Debug)]
+struct SelfResponse {
+ crypto_box_public_key: [u8; 32],
+ #[serde_as(as = "[_; 96]")]
+ bls_public_key: [u8; 96],
+ #[serde_as(as = "[_; 48]")]
+ bls_proof_of_possession: [u8; 48],
+ base_url: Option<String>,
+ build_id: String,
+}
+
+#[backend]
+pub async fn turbonet_self() -> SelfResponse {
  select!(_Turbonet_Self).unwrap().into()
+}
+
+impl _Turbonet_Peer {
+ async fn heartbeat(self) {
+  let mut peer = if self.rowid.is_some() {
+   self
+  } else if let Some(peer) =
+   select!(Option<_Turbonet_Peer> "WHERE ip = ?", self.ip.unwrap()).unwrap()
+  {
+   peer
+  } else {
+   _Turbonet_Peer { rowid: Some(self.insert().unwrap()), ..self }
+  };
+
+  peer.last_request_ms = Some(now_millis());
+  peer.update().unwrap();
+
+  let response = remote_turbonet_self(&format!(
+   "{}:{}",
+   std::net::Ipv4Addr::from(peer.ip.unwrap()),
+   peer.port.unwrap()
+  ))
+  .await;
+
+  peer.crypto_box_public_key = Some(response.crypto_box_public_key);
+  peer.bls_public_key = Some(response.bls_public_key);
+  peer.bls_proof_of_possession = Some(response.bls_proof_of_possession);
+  peer.build_id = Some(response.build_id);
+  peer.base_url = response.base_url;
+  peer.last_response_ms = Some(now_millis());
+  peer.update().unwrap();
+ }
 }
 
 /// Spawn a new Turbonet server. Future resolves when the server is ready to accept connections.
 pub async fn spawn_server(build_id: &str) -> Result<(), Box<dyn std::error::Error>> {
  let mut turbonet_self = select!(Option<_Turbonet_Self>)?.unwrap_or_else(|| {
-  let turbonet_self = _Turbonet_Self::generate();
+  let turbonet_self = _Turbonet_Self::generate_keys();
   turbonet_self.insert().unwrap();
   turbonet_self
  });
@@ -124,20 +149,9 @@ pub async fn spawn_server(build_id: &str) -> Result<(), Box<dyn std::error::Erro
 
   tokio::spawn(async move {
    loop {
-    let mut peer: _Turbonet_Peers =
-     remote_turbonet_self(&format!("{}:34254", TURBONET_BOOTSTRAP_IP.flag)).await.into();
-    peer.last_seen_ms =
-     Some(std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_millis()
-      as i64);
-    // if we have the peer, update it, otherwise insert it
-    if let Some(_Turbonet_Peers { rowid, .. }) =
-     select!(Option<_Turbonet_Peers> "WHERE ip = ?", ip).unwrap()
-    {
-     peer.rowid = rowid;
-     peer.update().unwrap();
-    } else {
-     peer.insert().unwrap();
-    }
+    _Turbonet_Peer { ip: Some(ip), port: Some(TURBONET_BOOTSTRAP_PORT.flag), ..Default::default() }
+     .heartbeat()
+     .await;
     tokio::time::sleep(tokio::time::Duration::from_secs(
      TURBONET_HEARTBEAT_INTERVAL_SECONDS.flag.into(),
     ))
